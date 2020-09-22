aef814f6cab532670ceaf8ca18ff4932899452af
Fabi <fabicaneyu@gmail.com>
2020-09-22 12:36:11 -0300

    Finalizado exercício dia 22/09

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
-app.get('/', function(request, response){
-    response.send('GET Hello World!');
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
+// arrial function é anônima: () => {}
+//se 1 parâmetro tira até os parenteses
+//request, response = req, res
+
+//endpoint - consultar no postmam GET: localhost:3000/
+app.get('/', (req, res) => {
+    res.send('GET Hello World!');
 })
 
-app.post('/', function (request,response){
-    response.send('POST Hello world!');
+//[[ Endpoints ]] de envios de mensagens
+//CRUD -> create, read(read all e read single), update and delete
+// crud criar, ler(ler tudo e ler individualmente), atualizar e remover
+
+const mensagens =[
+    "Essa é uma mensagem",
+    "Essa é outra mensagem"
+];
+
+//READ ALL
+//endpoint - consultar no postmam GET: localhost:3000/mensagens
+app.get('/mensagens', (req, res) => {
+    res.json(mensagens);
 })
 
-app.listen(port, function(){
+
+
+//Create
+//endpoint - consultar no postmam POST: localhost:3000/mensagens
+app.post('/mensagens', (req, res) => {
+
+    const mensagem = req.body.mensagem;
+    mensagens.push(mensagem);
+
+    const id = mensagens.length - 1;
+
+    res.send(`Mensagem criada ID: ${id}.`);
+})
+
+//Read single
+////endpoint - consultar no postmam GET: localhost:3000/mensagens/1 ou 2 
+app.get('/mensagens/:id', (req, res) => {
+    res.send('Exibe uma mensagem selecionada pelo ID que foi informado');
+})
+
+//Update
+//endpoint - consultar no postmam PUT: localhost:3000/mensagens/1 ou 2
+app.put('/mensagens/:id', (req, res) => {
+    res.send('Atualiza uma mensagem selecionada pelo ID que foi informado');
+})
+
+//Delete
+//endpoint - consultar no postmam DELETE: localhost:3000/mensagens/1 ou 2
+app.delete('/mensagens/:id', (req, res) => {
+    res.send('Remove uma mensagem selecionada pelo ID que foi informado');
+})
+
+//COMUNICAÇÃO com a porta 
+app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
 } )
